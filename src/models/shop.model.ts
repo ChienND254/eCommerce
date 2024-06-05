670eb45cac8dc36e22a2606fec9507d66942b847
chiennd <104510473+ChienND254@users.noreply.github.com>
2024-06-05 23:41:15 +0700

    add model shop

---
+++ src/models/shop.model.ts
@@ -0,0 +1,55 @@
+import {model, Schema, Document} from 'mongoose'; // Erase if already required
+
+interface IShop extends Document {
+    name: string;
+    email: string;
+    mobile: string;
+    password: string;
+    status: 'active' | 'inactive';
+    verify: boolean;
+    role: string[];
+}
+
+const DOCUMENT_NAME:string = 'Shop'
+const COLLECTION_NAME: string = 'Shops' 
+// Declare the Schema of the Mongo model
+var shopSchema = new Schema({
+    name:{
+        type:String,
+        trim: true,
+        maxLength: 150
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'inactive'
+    },
+
+    verify: {
+        type: Schema.Types.Boolean,
+        default: false
+    },
+    role: {
+        type: [String],
+        default: []
+    }
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+});
+
+export default model<IShop>(DOCUMENT_NAME, shopSchema);

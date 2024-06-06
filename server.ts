e092210ffcce4f9bf4e2dfe6d75dc3918de26942
ndchien254 <114422476+ndchien254@users.noreply.github.com>
2024-06-06 09:18:52 +0700

    Remove

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import app from './src/index'
+import app from './src/app'
 const PORT = process.env.PORT || 3055
 const server = app.listen(PORT, () => {
     console.log(`The application is listening on port ${PORT}!`);
